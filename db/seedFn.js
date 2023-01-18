310e8a2d5fc1cf44d09c3c975737847dd9e10f12
kersh2000 <bradleykershaw333@gmail.com>
2023-01-18 14:21:37 +0000

    Created Recipe entries and added relationships.

--- db/seedFn.js
+++ db/seedFn.js
@@ -1,11 +1,13 @@
 const {sequelize} = require('./db');
-const {User} = require('./');
-const users = require('./seedData');
+const {User, Recipe} = require('./');
 const bcrypt = require('bcrypt')
+const data = require('./seedData');
 
 const SALT_COUNT = 5
 
 const seed = async () => {
+  const users = data["users"]
+
   await sequelize.sync({ force: true }); // recreate db
   const promises = users.map(async (user) => {
     const hash = await bcrypt.hash(user.password, SALT_COUNT);
@@ -13,7 +15,25 @@ const seed = async () => {
     return user;
   });
   const hashedUsers = await Promise.all(promises);
-  await User.bulkCreate(hashedUsers);
+  const createdUsers = await User.bulkCreate(hashedUsers);
+  await addConnections(createdUsers)
 };
+
+async function addConnections(users) {
+  const recipes = data["recipes"]
+  const cons = data["connections"]
+
+  recipes.forEach(recipe => {
+    recipe.ingredients = JSON.stringify(recipe.ingredients);
+    recipe.steps = JSON.stringify(recipe.steps);
+  });
+
+  const createdRecipes = await Recipe.bulkCreate(recipes)
+
+  cons.forEach(async(con) => {
+    const usersRecipes = con["recipe_indices"].map(index => { return createdRecipes[index] })
+    await users[con["user_index"]].addRecipes(usersRecipes)
+  })
+}
 
 module.exports = seed;

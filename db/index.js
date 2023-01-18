5d263d4cc86b370ceb52afd37a761caaef3d7a5a
kersh2000 <bradleykershaw333@gmail.com>
2023-01-18 14:18:43 +0000

    Established relationships between Recipe and User.

--- db/index.js
+++ db/index.js
@@ -1,8 +1,13 @@
 const {User} = require('./User');
+const {Recipe} = require('./Recipe')
 const {sequelize, Sequelize} = require('./db');
+
+User.belongsToMany(Recipe, {through: 'User_Recipe'})
+Recipe.belongsToMany(User, {through: 'User_Recipe'})
 
 module.exports = {
     User,
+    Recipe,
     sequelize,
     Sequelize
 };
